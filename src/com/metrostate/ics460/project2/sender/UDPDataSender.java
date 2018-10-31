16648ed8a4ee0ad0e5c55ac534fbe2153d292374
Sennat <dinksira@gmail.com>
2018-10-31 17:28:03 -0500

    Change On UDPDataSender Class

--- src/com/metrostate/ics460/project2/sender/UDPDataSender.java
+++ src/com/metrostate/ics460/project2/sender/UDPDataSender.java
@@ -22,11 +22,12 @@ import com.metrostate.ics460.project2.Packet;
 public class UDPDataSender implements DataSender {
 
 	// Datagram for sent packet
-	private DatagramPacket send_packet = null;
+	private DatagramPacket sendPacket = null;
 	// Datagram for received packet
-	private DatagramPacket recieve_packet = null;
+	private DatagramPacket recievePacket = null;
 	// Probability of loss during packet sending
-	private static final double PROBABILITY = 0.1;
+	private static final double ERROR_PROBABILITY = 0.1;
+	private int totalPackets = 0;
 	// Sequence number of the last packet sent
 	private int seqNum = 0;
 	// Sequence number of the last acknowledged packet
@@ -38,7 +39,6 @@ public class UDPDataSender implements DataSender {
 	// The queue for message
 	private ArrayBlockingQueue<Packet> queue = new ArrayBlockingQueue(packetSeq, false);
 
-	// TODO change all variables with underscores to camel case.  So  packet_size to pacetkSize and window_size to windowSize to conform to java naming standards
 	@Override
 	public void sendData(byte[] bytes, int packet_size, int timeout, String ipAddress, int port, int windowSize) {
 
@@ -51,12 +51,8 @@ public class UDPDataSender implements DataSender {
 
 		// Get an input file data
 		FileLoader file_loader = new FileLoader();
+		// Add byte[] into a list by split byte to smaller chunks byte 
 		List<byte[]> byteList = byteArrayToChunks(file_loader.loadData(), packet_size);
-
-		sent_packet_list = byteListToPacketList(byteList);
-
-		// Array to store serilized packet to send
-		List<byte[]> bytes_to_send = SerializeObject.serializePacketObject((Packet) sent_packet_list);
 
 		// Create a datagram socket
 		try (DatagramSocket socket = new DatagramSocket(port)) {
@@ -67,44 +63,50 @@ public class UDPDataSender implements DataSender {
 			InetAddress host_ip = InetAddress.getByName(ipAddress);
 
 			// Create a buffer to store the incoming datagrams packets
-			for (byte[] packet_out : bytes_to_send) {
+			for (byte[] bytes_to_send : byteList) {
 
 				// Packet sending while loop
 				while (seqNum - waitingForAck < windowSize && seqNum < packetSeq) {
-
-					// Array to store a part of the bytes to send
-					byte[] arrayToSend = new byte[packet_size];
 					
+					// Convert byteList to a List of Packet Object
+					sent_packet_list = byteListToPacketList(byteList);
+					// Total number of packets
+					
+					// A byte array to store serialized packet to send
+					byte[] packet_out = SerializeObject.serializePacketObject((Packet) sent_packet_list);
+					
+					totalPackets = sent_packet_list.size();
+	
 					// Create datagram packet to send
 					DatagramPacket sendPacket = new DatagramPacket(packet_out, packet_out.length, host_ip, port);
 					System.out.println("Sending packet with sequence number " + packetSeq + " and size "
 							+ sendPacket.getLength() + " bytes");
 					
+					// Send with some bit error probability of bit losses
+					if (Math.random() > ERROR_PROBABILITY) {
+						socket.send(sendPacket);
+
+					} else {
+
+						System.out.println("[X] Lost packet with sequence number " + packetSeq);
+					}
+
 					// Copy a part of the bytes to send into byte array
-					arrayToSend = Arrays.copyOfRange(packet_out, seqNum * packet_size, seqNum * packet_size + packet_size); 
+					byte[] sentPacketCopy = Arrays.copyOfRange(packet_out, seqNum * packet_size,
+							seqNum * packet_size + packet_size);
 
 					// Send a packet
 					socket.send(sendPacket);
-					
 
-					// Send with some probability of loss
-					if (Math.random() > PROBABILITY) {
-						socket.send(sendPacket);
-						
-					} else {
-						
-						System.out.println("[X] Lost packet with sequence number " + packetSeq);
-					}
-					
-					// Increase the last sent
+					// Increase the last sent sequence number
 					seqNum++;
 
 				}
-				
-				// Byte array for the ACK sent by the receiver
-				byte[] ack_bytes = new byte[500];
 
-				// Creating packet for acknowledgment
+				// Byte array for acknowledged packet object sent by the receiver
+				byte[] ack_bytes = new byte[0];
+
+				// Creating datagram packet for acknowledgment
 				DatagramPacket ackPacket = new DatagramPacket(ack_bytes, ack_bytes.length);
 
 				try {
@@ -115,36 +117,38 @@ public class UDPDataSender implements DataSender {
 					socket.receive(ackPacket);
 
 					// deserialize the acknowledged packet object
-					Packet acknPacket = (Packet) SerializeObject.deserializePacketObject(ackPacket.getData());
+					Packet acknPacket = SerializeObject.deserializePacketObject(ack_bytes);
 
 					System.out.println("Received ACK for " + acknPacket.getAckno());
 
 					// If this acknowledged is for the last packet, stop the sender
 					if (acknPacket.getAckno() == packetSeq) {
+						
 						break;
 					}
 
 					waitingForAck = Math.max(waitingForAck, acknPacket.getAckno());
 
-				} catch (SocketTimeoutException e) {
+				} catch (SocketTimeoutException | ClassNotFoundException e) {
 					// Re-send all non-acknowledged packets
 					for (int i = waitingForAck; i < seqNum; i++) {
 
-						// Serialize9 the Packet object
-						List<byte[]> sendData = SerializeObject.serializePacketObject(sent_packet_list.get(i));
+						// Serialize the Packet object
+						byte[] sendData = SerializeObject.serializePacketObject(sent_packet_list.get(i));
 
 						// Create the packet
 						DatagramPacket packet = new DatagramPacket(sendData, sendData.length, host_ip, port);
 
 						// Send with some probability
-						if (Math.random() > PROBABILITY) {
+						if (Math.random() > ERROR_PROBABILITY) {
 							try {
 								socket.send(packet);
 							} catch (IOException e1) {
 								e1.printStackTrace();
 							}
 						} else {
-							System.out.println("[X] Lost packet with sequence number " + sent_packet_list.get(i).getSeqno());
+							System.out.println(
+									"[X] Lost packet with sequence number " + sent_packet_list.get(i).getSeqno());
 						}
 
 						System.out.println("Resending packet with sequence number " + sent_packet_list.get(i).getSeqno()
@@ -179,15 +183,15 @@ public class UDPDataSender implements DataSender {
 
 	}
 
-	// Convert byteList to a List<Packet Object>.
-	private static List<Packet> byteListToPacketList(List<byte[]> byteList){
+	// Convert byteList to a List<Packet Object>
+	private static List<Packet> byteListToPacketList(List<byte[]> byteList) {
 		List<Packet> packet_list = new ArrayList<Packet>();
-		for(int i = 0; i < byteList.size(); i++) {
+		for (int i = 0; i < byteList.size(); i++) {
 			Packet packet = new Packet();
-			byte[] bytes =  byteList.get(i);
+			byte[] bytes = byteList.get(i);
 			packet.setCksum((short) 0);
 			packet.setLen((short) (bytes.length + 12));
-			packet.setSeqno(i+1);
+			packet.setSeqno(i + 1);
 			packet.setData(bytes);
 
 			packet_list.add(packet);
@@ -197,13 +201,11 @@ public class UDPDataSender implements DataSender {
 
 	}
 
-
 	// Private class for Serializing Object
 	private static class SerializeObject {
 
 		// Convert an object to byte array
-		private static List<byte[]> serializePacketObject(Packet packet) {
-			List<byte[]> data = new ArrayList<>();
+		private static byte[] serializePacketObject(Packet packet) {
 			ByteArrayOutputStream bos = new ByteArrayOutputStream();
 			ObjectOutputStream oos = null;
 			try {
@@ -226,34 +228,19 @@ public class UDPDataSender implements DataSender {
 				e.printStackTrace();
 			}
 
-			data.add(bos.toByteArray());
+			byte[] bytes = bos.toByteArray();
 
-			return data;
+			return bytes;
 		}
 
-		public static Packet deserializePacketObject(byte[] data) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	private static Packet deserializePacketObject(byte[] ackPacket) throws ClassNotFoundException, IOException {
+		ByteArrayInputStream bais = new ByteArrayInputStream(ackPacket);
+		ObjectInputStream ois = new ObjectInputStream(bais);
+		
+		return (Packet) ois.readObject();
 
+	}
 	}
 	
-	private static byte[] deserializePacketObject(byte[] bytes) {
-		ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-		ObjectInputStream ois = null;
-		try {
-			ois = new ObjectInputStream(bais);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			return (byte[]) ois.readObject();
-		} catch (ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return bytes;
-	}
 
 }
