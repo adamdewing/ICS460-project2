8837b1d0f8f6865b100f34823233ad071284b516
Sennat <dinksira@gmail.com>
2018-10-31 22:34:03 -0500

    Update src/com/metrostate/ics460/project2/sender/UDPDataSender.java

--- src/com/metrostate/ics460/project2/sender/UDPDataSender.java
+++ src/com/metrostate/ics460/project2/sender/UDPDataSender.java
@@ -3,19 +3,14 @@ package com.metrostate.ics460.project2.sender;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
 
 import com.metrostate.ics460.project2.Packet;
 
@@ -27,15 +22,18 @@ public class UDPDataSender implements DataSender {
 	private DatagramPacket recievePacket = null;
 	// Probability of loss during packet sending
 	private static final double ERROR_PROBABILITY = 0.1;
-	private int totalPackets = 0;
+	private int totalPackets;
 	// Sequence number of the last packet sent
-	private int seqNum = 0;   // TODO seqNo starts at 1
+	private int seqNum = 1;
 	// Sequence number of the last acknowledged packet
 	private int waitingForAck = 0;  // TODO you need to initialize this to windowSize
 	// Last packet sequence number
 	private int packetSeq = 0;
-	// List for windows
-	private LinkedList<Packet> WindowsList;
+	// A position of packet is being sent which is (in windowBuffer)
+	private int windowIndex;
+	// Create an array of packet for slide window buffer
+	private Packet[] windowBuffer;
+
 
 	@Override
 	public void sendData(byte[] bytes, int packet_size, int timeout, String ipAddress, int port, int windowSize) {
@@ -44,11 +42,14 @@ public class UDPDataSender implements DataSender {
 		System.out.println("\t\tClient Started To Send Data");
 		System.out.println("+ ======================================================= +");
 
-		// List of all the packets to sent not yet been acknowledged in a buffer
+		// List of all the packets to sent which is not yet been acknowledged in a buffer
 		List<Packet> sent_packet_list = new ArrayList<Packet>();
+		// Total number of packets
+		totalPackets = sent_packet_list.size();
 
 		// Get an input file data
 		FileLoader file_loader = new FileLoader();
+
 		// Add byte[] into a list by split byte to smaller chunks byte 
 		List<byte[]> byteList = byteArrayToChunks(file_loader.loadData(), packet_size);
 
@@ -60,108 +61,39 @@ public class UDPDataSender implements DataSender {
 			// Receiver address
 			InetAddress host_ip = InetAddress.getByName(ipAddress);
 
-			// Create a buffer to store the incoming datagrams packets
-			for (byte[] bytes_to_send : byteList) {  // TODO You just want to do a while(true) loop until you get the Ack Packet back for the last Packet
+			// Convert byteList to a List of Packet Object
+			sent_packet_list = byteListToPacketList(byteList);
 
-				// Packet sending while loop
-				while (seqNum - waitingForAck < windowSize && seqNum < packetSeq) {
-					
-					// Convert byteList to a List of Packet Object
-					sent_packet_list = byteListToPacketList(byteList);  // TODO you are doing this in a loop.  This needs to be outside the loop.
-					// Total number of packets
-					
-					// A byte array to store serialized packet to send
-					byte[] packet_out = SerializeObject.serializePacketObject((Packet) sent_packet_list);  // TODO you are serializing the entire list.  You only want to send one Packet object with each DatagramPacket
+			// A byte array to store serialized packet to send
+			byte[] packet_out = SerializeObject.serializePacketObject((Packet) sent_packet_list);
 
-					totalPackets = sent_packet_list.size();
-	
-					// Create datagram packet to send
-					DatagramPacket sendPacket = new DatagramPacket(packet_out, packet_out.length, host_ip, port);
-					System.out.println("Sending packet with sequence number " + packetSeq + " and size "
-							+ sendPacket.getLength() + " bytes");
-					
-					// Send with some bit error probability of bit losses
-					if (Math.random() > ERROR_PROBABILITY) {
-						socket.send(sendPacket);
+			while(true) {
+				// Create datagram packet to send
+				sendPacket = new DatagramPacket(packet_out, packet_out.length, host_ip, port);
 
-					} else {
+				// Create an array of packet for slide window buffer
+				windowBuffer = new Packet[windowSize];
 
-						System.out.println("[X] Lost packet with sequence number " + packetSeq);
-					}
+				while(totalPackets >= windowSize) {
 
-					// TODO removed unused code
-					// Copy a part of the bytes to send into byte array
-					byte[] sentPacketCopy = Arrays.copyOfRange(packet_out, seqNum * packet_size,
-							seqNum * packet_size + packet_size);
+					// Create packet with its index position
+					Packet packetPosition = new Packet();
+					windowBuffer[seqNum] = packetPosition;
 
-					// Send a packet
-					socket.send(sendPacket);
-
-					// Increase the last sent sequence number
+					// Subtract window size from a total packet
+					totalPackets = totalPackets - windowSize;
 					seqNum++;
 
-				}
-
-				// Byte array for acknowledged packet object sent by the receiver
-				byte[] ack_bytes = new byte[0];    // TODO you don't know how big the Ack will be, but it WILL be greater than 0.  I would just use 1024.
-
-				// Creating datagram packet for acknowledgment
-				DatagramPacket ackPacket = new DatagramPacket(ack_bytes, ack_bytes.length);
-
-				try {
-					// Timeout will trigger if an acknowledgement was not received in the time specified
-					socket.setSoTimeout(timeout);  // TODO you already set the socket SoTimeout
-
-					// TODO you want to send all the Packet objects that need to be sent in the window.
-					// TODO here you are sending only one Packet and then waiting.  This is NOT the sliding window way
-					// Receive the packet
-					socket.receive(ackPacket);
-
-					// deserialize the acknowledged packet object
-					Packet acknPacket = SerializeObject.deserializePacketObject(ack_bytes);
-
-					System.out.println("Received ACK for " + acknPacket.getAckno());
-
-					// If this acknowledged is for the last packet, stop the sender
-					if (acknPacket.getAckno() == packetSeq) {
-						
+					if(windowIndex == totalPackets) {
 						break;
 					}
-
-					waitingForAck = Math.max(waitingForAck, acknPacket.getAckno());
-
-				} catch (SocketTimeoutException | ClassNotFoundException e) {
-					// TODO you only want to send the Packet that timed out, not all DatagramPackets
-					// Re-send all non-acknowledged packets
-					for (int i = waitingForAck; i < seqNum; i++) {
-
-						// Serialize the Packet object
-						byte[] sendData = SerializeObject.serializePacketObject(sent_packet_list.get(i));
-
-						// Create the packet
-						DatagramPacket packet = new DatagramPacket(sendData, sendData.length, host_ip, port);
-
-						// Send with some probability
-						if (Math.random() > ERROR_PROBABILITY) {
-							try {
-								// TODO remove ALL error code.  Need to get things working first!!!
-								socket.send(packet);
-							} catch (IOException e1) {
-								e1.printStackTrace();
-							}
-						} else {
-							System.out.println(
-									"[X] Lost packet with sequence number " + sent_packet_list.get(i).getSeqno());
-						}
-
-						System.out.println("Resending packet with sequence number " + sent_packet_list.get(i).getSeqno()
-								+ " and size " + sendData.length + " bytes");
-					}
 				}
+				// Send all the packet in windowBuffer
+				for (int j = 0; j <= windowIndex; j++) {
+					System.out.println("Sending " + packet_out + "  " + packet_out.length + " bytes to client " + windowBuffer[j]);
 
+				}
 			}
-
-			System.out.println("Finished transmission");
 
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
@@ -236,14 +168,14 @@ public class UDPDataSender implements DataSender {
 			return bytes;
 		}
 
-	private static Packet deserializePacketObject(byte[] ackPacket) throws ClassNotFoundException, IOException {
-		ByteArrayInputStream bais = new ByteArrayInputStream(ackPacket);
-		ObjectInputStream ois = new ObjectInputStream(bais);
-		
-		return (Packet) ois.readObject();
+		private static Packet deserializePacketObject(byte[] ackPacket) throws ClassNotFoundException, IOException {
+			ByteArrayInputStream bais = new ByteArrayInputStream(ackPacket);
+			ObjectInputStream ois = new ObjectInputStream(bais);
 
+			return (Packet) ois.readObject();
+
+		}
 	}
-	}
-	
+
 
 }
